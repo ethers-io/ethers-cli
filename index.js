1b5dceaf214143855b0cc8104ce57cb9c525fb8c
Richard Moore <me@ricmoo.com>
2017-10-23 01:27:29 -0400

    Large refactor of ethers-cli; includes better testing and debugging environment.

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 'use strict';
 
+var builders = require('./lib/builders');
+
 module.exports = {
-    Slug: require('./lib/slug.js')
+    builders: builders,
+    Builder: builders.Builder,
+    TestBuilder: builders.TestBuilder,
+
+    compiler: require('./lib/compiler'),
+
+    Slug: require('./lib/slug'),
+    TestProvider: require('./lib/test-provider'),
 }
